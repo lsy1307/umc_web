5635b23edfa535945cf35ed66af1b8ac9512a3b4
lsy1307 <lsy1307@inha.edu>
2024-04-03 18:27:25 +0900

    수정#8

--- chapter1/counter/main.js
+++ chapter1/counter/main.js
@@ -1,25 +1,18 @@
-            var number = document.querySelector('#number');
-            var increase = document.querySelector('#increase');
-            var decrease = document.querySelector('#decrease');
-            /*
-            console.log(number);
-            console.log(increase);
-            console.log(decrease);
-            */
+            var number = document.getElementById("number");
+            var increase = document.getElementById("increase");
+            var decrease = document.getElementById("decrease");
             var elements = [number, increase, decrease];
+            var number_val = parseInt(number.textContent, 10);
             elements.forEach(function(element){
                 console.log(element);
             });
-            var number_val = parseInt(number.textContent, 10);
-            var increase_eve = document.getElementById("increase")
-            var decrease_eve = document.getElementById("decrease")
 
-            increase_eve.addEventListener('click', function() {
+            increase.addEventListener('click', function() {
                 number_val += 1;
                 number.textContent = number_val;
                 console.log('increase가 클릭 됨')
             })
-            decrease_eve.addEventListener('click', function() {
+            decrease.addEventListener('click', function() {
                 number_val -= 1;
                 number.textContent = number_val;
                 console.log('decrease가 클릭 됨')
