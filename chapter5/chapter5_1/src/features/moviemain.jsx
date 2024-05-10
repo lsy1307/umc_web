3b35bbb22d2105a5ed4f564de651b12400a6764c
lsy1307 <lsy1307@inha.edu>
2024-05-10 19:51:54 +0900

    수정1

--- chapter5/chapter5_1/src/features/moviemain.jsx
+++ chapter5/chapter5_1/src/features/moviemain.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import axios from "axios";
 import search from "../assets/search.svg";
 import movie from "../assets/movie.svg";
+import { getAPI } from "../config.js";
+import Loading from "../components/Loading.jsx";
 
 const Container = styled.div`
   height: 100%;
@@ -39,12 +42,6 @@ const MovieIcon = styled.img`
   top: 5px;
 `;
 
-const MovieText = styled.span`
-  vertical-align: middle;
-  margin-left: 10px;
-  color: #e3fef7;
-`;
-
 const SearchBarContainer = styled.div`
   width: 30%;
   display: flex;
@@ -69,6 +66,102 @@ const SearchButton = styled.button`
   }
 `;
 
+const SearchContainer = styled.div`
+  height: 50%;
+  width: 70%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 20px;
+  padding: 150px 40px;
+  display:none;
+`;
+const ContentContainer = styled.div`
+  background-color: #77b0aa;
+  padding: 10px;
+  position: relative;
+
+  &:hover .movie-poster-container {
+    opacity: 0.1;
+  }
+`;
+
+const MovieOverview = styled.div`
+  position: absolute;
+
+  display: none;
+  color: #e3fef7;
+
+  padding: 10px;
+
+  z-index: 999;
+
+  word-wrap: break-word;
+
+  ${ContentContainer}:hover & {
+    display: block;
+  }
+`;
+
+const MoviePoster = styled.img`
+  width: 100%;
+  padding: 5px;
+  box-sizing: border-box;
+  z-index: 1;
+`;
+
+const MovieData = styled.div`
+  color: e3fef7;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding-bottom: 10px;
+`;
+
+const OverView = styled.p`
+  padding-top: 30px;
+`;
+
+const MovieText = styled.div`
+  font-weight: bold;
+  color: #e3fef7;
+`;
+
+const [searchData, setSearch] = useState("");
+const [isButton, setButton] = useState(false);
+const [movieData, setMovieData] = useState([]);
+const [isLoading, setIsLoading] = useState(true);
+  const inputHandler = (e) => {
+    setSearch(e.target);
+  };
+
+useEffect(() => {
+  const getMovieData = async () => {
+    const option = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${import.meta.env.VITE_TMDB_ACCESS_TOKEN}`,
+      },
+      params: {
+        query: searchData,
+        include_adult: true,
+        language: "ko-KR",
+        page: "1",
+      },
+      url: `https://api.themoviedb.org/3/search/movie`,
+    };
+    try {
+      setIsLoading(true);
+      const response = await axios(option);
+      setMovieData(response.data.results);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  getMovieData();
+}, isButton);
+
 const Main = () => {
   return (
     <Container>
@@ -79,11 +172,34 @@ const Main = () => {
           <MovieText>Find Your Movies!</MovieText>
         </div>
         <SearchBarContainer>
-          <SearchInput type="text" placeholder="영화 제목을 입력해주세요" />
-          <SearchButton>
+          <SearchInput type="text" placeholder="영화 제목을 입력해주세요" onChange = {inputHandler()}/>
+          <SearchButton onClick={}>
             <img src={search}></img>
           </SearchButton>
         </SearchBarContainer>
+        <SearchContainer>
+          {movieData.map((movie, index) => (
+            <ContentContainer key={index}>
+              <div>
+                <MovieOverview className="movie-overview">
+                  <h2>{movie.title}</h2>
+                  <OverView>{movie.overview}</OverView>
+                </MovieOverview>
+                <div className="movie-poster-container">
+                  <MoviePoster
+                    src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                    alt={movie.title}
+                    className="movie-poster"
+                  />
+                </div>
+                <MovieData>
+                  <MovieText>{movie.title}</MovieText>
+                  <MovieText>{movie.vote_average}</MovieText>
+                </MovieData>
+              </div>
+            </ContentContainer>
+          ))}
+        </SearchContainer>
       </Search>
     </Container>
   );
